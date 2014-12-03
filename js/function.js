8a56bce368ae5d923f3b6ca14a9bf1a36788417a
iHectoR <HectoR.polt@gmail.com>
2014-12-03 17:22:20 +0200

    animated accordion

--- js/function.js
+++ js/function.js
@@ -2,14 +2,28 @@
  * Created by HectoR on 01.12.2014.
  */
 jQuery(function($){
+//    Display search field
     $('.search-icon').click(function(){
         $('.search').toggleClass('search-open');
     });
+//    Hide search field if empty
     $('.search-button').click(function(){
         if ($('.search-field').val().length==0){
         $('.search').toggleClass('search-open');
         return false;
         }
     });
-
+//  Show Menu
+    $('.menu-toggle').click(function(){
+        $('body').toggleClass('menu-open');
+//        $('.menu-open').css('height', $(window).width());
+    });
+    $('.menu>ul>li').hover(
+        function(){
+        $('>.wrap-menu',this).css('height', $('.sub-menu>li').size()*35+30);
+    },
+        function(){
+            $('>.wrap-menu',this).css('height', '0');
+    }
+    );
 });
